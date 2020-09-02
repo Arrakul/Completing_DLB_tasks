3ecf2892904c31547a243906552c64f0ea401f95
Lesha Losev <Losev_@openmygame.com>
2020-09-02 21:18:28 +0300

    the Brackets-519 task was completed

---
+++ Brackets-519.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Brackets_519
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteAnswer(Algoritm(ReadData()));
+        }
+
+        public static string ReadData()
+        {
+            string Lines = string.Empty;
+
+            if (!File.Exists("input.txt")) return null;
+
+            FileStream file = new FileStream("input.txt", FileMode.Open);
+
+            using (StreamReader st = new StreamReader(file))
+            {
+                string line;
+
+                while ((line = st.ReadLine()) != null)
+                {
+                    Lines = line;
+                }
+            }
+
+            file.Close();
+            return Lines;
+        }
+
+        public static void WriteAnswer(bool answer)
+        {
+            FileStream file = new FileStream("output.txt", FileMode.Create);
+
+            string ans = (answer) ? "Да" : "Нет";
+
+            using (StreamWriter st = new StreamWriter(file))
+            {
+                st.WriteLine(ans);
+            }
+
+            file.Close();
+        }
+
+        public static bool Algoritm(string line)
+        {
+            if (line.Length % 2 == 0)
+            {
+                Stack<char> stack = new Stack<char>();
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if(line[i] == '(' || line[i] == '[' || line[i] == '{')
+                    {
+                        stack.Push(line[i]);
+                    }
+                    else
+                    {
+                        if (stack.Count == 0) return false;
+
+                        char close = stack.Pop();
+
+                        if (close == '(' && line[i] != ')' ||
+                            close == '[' && line[i] != ']' ||
+                            close == '{' && line[i] != '}') return false;
+                    }
+                }
+
+                if (stack.Count != 0) return false;
+                else return true;
+            }
+
+            return false;
+        }
+    }
+}
