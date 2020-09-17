ca2fe3423be4775645e8c27bfba8b5907b7dd87c
Arrakul <58705459+Arrakul@users.noreply.github.com>
2020-09-17 13:49:14 +0300

    Update TheDecompositionNumber_Amount-2289.cs

--- TheDecompositionNumber_Amount-2289.cs
+++ TheDecompositionNumber_Amount-2289.cs
@@ -1,49 +1,12 @@
-﻿using System;
-using System.IO;
+using System;
 
 namespace TheDecompositionNumber_Amount_2289
 {
     class Program
     {
-        static int N;
-
         static void Main(string[] args)
         {
-            WriteAnswer(Calc(Convert.ToInt64(ReadData())));
-        }
-
-        public static string ReadData()
-        {
-            string Line = string.Empty;
-
-            if (!File.Exists("input.txt")) return null;
-
-            FileStream file = new FileStream("input.txt", FileMode.Open);
-
-            using (StreamReader st = new StreamReader(file))
-            {
-                string line;
-
-                while ((line = st.ReadLine()) != null)
-                {
-                    Line = line;
-                }
-            }
-
-            file.Close();
-            return Line;
-        }
-
-        public static void WriteAnswer(long kol_element)
-        {
-            FileStream file = new FileStream("output.txt", FileMode.Create);
-
-            using (StreamWriter st = new StreamWriter(file))
-            {
-                st.WriteLine(kol_element);
-            }
-
-            file.Close();
+            Console.WriteLine(Calc(Convert.ToInt64(Console.ReadLine())));
         }
 
         public static long Calc(long num)
